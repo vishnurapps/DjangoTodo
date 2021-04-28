20c55ec1c8b14aafe5fb02f3af5827c8f8fcd7aa
Vishnu R <vishnurapps@gmail.com>
2021-04-28 08:02:43 +0530

    clean up code and update post

--- todolist/views.py
+++ todolist/views.py
@@ -19,20 +19,13 @@ def index(request):
     if request.method == 'GET':
         todos = TodoList.objects.all()
         serializer = TodoListSerializer(todos, many=True)
-        # print(type(todos[0].due))
-        # tasks = [("id : "+ str(i.id), "task name : "+i.task, "priority : "+ str(i.priority), "due date : " + str(i.due)) for i in todos]
-        # for task in todos:
-        #     serialized_obj = serializers.serialize('json', [ task, ])
-        #     print(serialized_obj)
-        # return JsonResponse({"task list" : tasks})
         return Response(serializer.data)
     elif request.method == 'POST':
-        todo_data = JSONParser().parse(request)
-        todo_serializer = TodoListSerializer(data=todo_data)
+        todo_serializer = TodoListSerializer(data=request.data)
         if todo_serializer.is_valid():
             todo_serializer.save()
-            return JsonResponse(todo_serializer.data, status=status.HTTP_201_CREATED) 
-        return JsonResponse(todo_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(todo_serializer.data, status=status.HTTP_201_CREATED) 
+        return Response(todo_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
         print(request.data['id'])
         print(type(request.data['id']))
